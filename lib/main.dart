af7513d97952c5ec2d6817ad49010609a05224cc
Renan Rocha <renanrochamorais@outlook.com>
2023-06-14 21:18:20 -0300

    Configuração inicial do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:sticker_swap_client/src/app.dart';
 import 'package:sticker_swap_client/src/app_module.dart';
 
-void main() async{
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ModularApp(module: AppModule(), child: App(),));
 }
